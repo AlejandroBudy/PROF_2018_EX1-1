847ac709b41fcf61e5bf63569e236edec1e86b19
alejandro torres <atorresato@gmail.com>
2018-11-20 16:59:26 +0100

    Test implemented

--- test/MyStringTest.java
+++ test/MyStringTest.java
@@ -1,2 +1,39 @@
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class MyStringTest {
+
+    MyString myString;
+    @Before
+    public void setUp(){
+        myString = new MyString("prueba");
+    }
+
+    @Test
+    public void shouldReturnGTZero(){
+        int result = this.myString.compareTo("pru");
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void shouldReturnLTZero(){
+        int result = this.myString.compareTo("prueba1");
+        assertEquals(result, -1);
+    }
+
+    @Test
+    public void shouldReturnZero(){
+        int result = this.myString.compareTo("prueba");
+        assertEquals(result, 0);
+    }
+
+    @Test
+    public void shouldReturnOtherThanZero(){
+        int result = this.myString.compareTo("prheba");
+        assertFalse(result == 0);
+    }
 }
