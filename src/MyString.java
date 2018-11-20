5c1912843c7b8a04d6edef8a813579bbb137467a
alejandro torres <atorresato@gmail.com>
2018-11-20 16:38:11 +0100

    Implementation done

--- src/MyString.java
+++ src/MyString.java
@@ -1,2 +1,26 @@
 public class MyString {
+
+    private String myString;
+
+    public MyString(String myString){
+        this.myString = myString;
+    }
+    public int compareTo(String str){
+        int result = -1;
+
+        if(this.myString.length() > str.length()){
+            result = 1;
+        }
+        else if(this.myString.length() < str.length()){
+            result = -1;
+        }
+        else{
+            result = 0;
+            for(int i = 0; i < this.myString.length() && result == 0; i++){
+                result = this.myString.charAt(i) - str.charAt(i);
+            }
+        }
+        return result;
+
+    }
 }
